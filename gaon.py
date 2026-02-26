a5d5c5c3c491bb25b0d30060052d11f5a8d79fb6
Smiley337 <167200225+Smiley337@users.noreply.github.com>
2026-02-26 08:33:54 -0500

    gaon gets slash commands

--- gaon.py
+++ gaon.py
@@ -7,21 +7,28 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="/", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 #봇이 켜지면 되는거래요?
 @bot.event
 async def on_ready():
+    await bot.tree.sync()
     print(f'{bot.user} 로 로그인 성공!')
 """
 async def on_member_updat(before, after):
     ctx.send("{}님이 {}님이 되었어요!".format(before, after))
 """
+@bot.tree.command(name="hello", description="인사하기") #슬래시 커맨드에 tree라는걸로 등록이 됨
+async def hello(interaction: discord.Interaction): #interaction 부분이 중요함
+    await interaction.response.send_message("안녕하세요!")
+
 @bot.command(aliases=['ㅎㅇ', '가온아', 'hi']) #aliases는 안녕 대신에 쓸수있는 단어를 말함
 async def 안녕(ctx):
     await ctx.send("안녕하세요! 👋")
 
 bot.run(TOKEN)
+
+
 
 
 
