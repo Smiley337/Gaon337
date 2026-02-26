3d21aa4d4d61227d884975f3be1202d51ca3ab28
Smiley337 <167200225+Smiley337@users.noreply.github.com>
2026-02-26 08:55:07 -0500

    frrom call id to call name

--- gaon.py
+++ gaon.py
@@ -15,11 +15,10 @@ async def on_ready():
     await bot.tree.sync()
     print(f'{bot.user} 로 로그인 성공!')
 
-
 @bot.event #가온이가 본인 이름을 인식함
 async def on_message(message):
     if message.content.startswith("가온아"):
-        await message.channel.send("네에? {}님은 뭐하고 계시나요?".format(message.author))
+        await message.channel.send("네에?")
 
     await bot.process_commands(message)
 """
@@ -36,9 +35,10 @@ async def 안녕(ctx):
 
 @bot.command(aliases=['뭐해?', 'ㅁㅎ', '넌 뭐해?']) #aliases는 안녕 대신에 쓸수있는 단어를 말함
 async def 뭐해(ctx):
-    await ctx.send("일리님을 보조하고 있어요.. 피곤해요.")
+    await ctx.send("일리님을 보조하고 있어요.. 피곤해요. {}님은 뭐하고 계시나요?".format(ctx.name))
 
 bot.run(TOKEN)
+
 
 
 
