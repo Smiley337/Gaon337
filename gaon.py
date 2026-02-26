b2cabd1af3695667a5e5f130905efbfc1a57d0d8
Smiley337 <167200225+Smiley337@users.noreply.github.com>
2026-02-25 20:53:16 -0500

    Update gaon.py

--- gaon.py
+++ gaon.py
@@ -7,15 +7,25 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="가온아 ", intents=intents)
 
 #봇이 켜지면 되는거래요?
 @bot.event
 async def on_ready():
     print(f'{bot.user} 로 로그인 성공!')
     
-@bot.command()
+""" @bot.command()
 async def 안녕(ctx):
     await ctx.send("안녕하세요! 👋")
+ """
+@bot.command(name="안녕", help="인사말", aliases=["인사", "하이"])
+async def hello(ctx):
+  hi = random.randrange(1,4)
+  if hi == 1:
+    await ctx.channel.send("안녕하세요")
+  elif hi == 2:
+    await ctx.channel.send("안녕")
+  elif hi == 3:
+    await ctx.channel.send("네, 안녕하세요")
 
 bot.run(TOKEN)
